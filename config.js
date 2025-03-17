6b65161c34655dee982363584a7e4543ce73a12d
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-03-17 16:12:13 +0530

    remove .env file

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const config = {
   // Weather and Gemini APIs
-  WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  WEATHER_API_KEY: '63ea7696193a8b454b2775d132cb2a50',
+  GEMINI_API_KEY: 'AIzaSyDOal6vThooDPnjXkvSnENNoh_GUERCEiQ',
   
   // Google Auth
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_ID: '642421121831-opeouvto6tq7g8umf7i36s60lblfjf62.apps.googleusercontent.com',
   
   // Firebase Config
   firebase: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAsD_5q9EfBh1ke7nS4ceuRR1w90uFPwe0",
+    authDomain: "weather-app-d8eeb.firebaseapp.com",
+    databaseURL: "https://weather-app-d8eeb-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "weather-app-d8eeb",
+    storageBucket: "weather-app-d8eeb.firebasestorage.app",
+    messagingSenderId: "899877398474",
+    appId: "1:899877398474:web:a46e671572bcc3be001133",
+    measurementId: "G-12MV40EKWP"
   }
 }; 
